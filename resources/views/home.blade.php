ac22d758612fac70ff11fdc18aeaf084eeff35eb
epalermo92 <emanuelepalermo1992@gmail.com>
2019-05-16 15:21:05 +0200

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="https://www.boolean.careers/images/favicon/favicon.ico">
+    <!-- Css -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>Il corso per diventare web developer e trovare un nuovo lavoro</title>
+  </head>
+  <body>
+
+    <div class="container">
+      @include('boolean.header')
+      @include('boolean.content')
+      @include('boolean.footer')
+    </div>
+
+
+  </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('boolean', function () {
+    return view('home');
+});
