b807b499fdfeeb2c2f7809b90b6fb23f0fd4e70a
epalermo92 <emanuelepalermo1992@gmail.com>
2019-05-16 17:43:04 +0200

    first part of content done

--- resources/views/boolean/content.blade.php
+++ resources/views/boolean/content.blade.php
@@ -1,3 +1,17 @@
 <div class="content">
-  CONTENT
+  <div class="become-developer">
+    <div class="left">
+      <div class="cont">
+        <h1>Diventa sviluppatore web</h1>
+        <p>6 mesi di <strong>corso intensivo online in diretta</strong>
+          per imparare da zero il mestiere di web developer e <strong>trovare un nuovo lavoro</strong>
+          nelle aziende top del settore
+        </p>
+        <a href="#">SCOPRI IL CORSO</a>
+      </div>
+    </div>
+    <div class="right">
+      <img src="https://www.boolean.careers/images/common/mac2x.png" alt="">
+    </div>
+  </div>
 </div>
--- resources/views/boolean/footer.blade.php
+++ resources/views/boolean/footer.blade.php
@@ -1,3 +1,2 @@
 <footer>
-  FOOTER
 </footer>
